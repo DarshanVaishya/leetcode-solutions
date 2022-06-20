d547d4a3a350326bc835a3f0928b9ab80a18dfd6
Darshan Vaishya <66417026+DarshanVaishya@users.noreply.github.com>
2022-06-20 12:43:42 +0530

    Time: 1 ms (69.83%), Space: 42.6 MB (53.24%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,56 +1,24 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length - 1;
-        int pivot = findPeak(nums);
-
-        // if pivot is -1 then there is no rotation
-        if(pivot == -1) {
-            return binarySearch(nums, target, 0, n);
-        }
-
-        if(nums[pivot] == target) {
-            return pivot;
-        }
-
-        if(target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot);
-        }
-
-        return binarySearch(nums, target, pivot + 1, n);
-    }
-    
-    public int findPeak(int[] nums) {
+    public int search(int[] arr, int target) {
         int start = 0;
-        int end   = nums.length - 1;
-
-        while(start < end) {
-            int mid = start + (end - start) / 2;
-
-            if(mid < end && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-            else if(mid > start && nums[mid - 1] > nums[mid]) {
-                return mid - 1;
-            }
-            else if(nums[mid] < nums[start]) {
-                end = mid - 1;
-            }
-            else {
-                start = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-    
-    public int binarySearch(int[] nums, int target, int start, int end) {
+        int end  = arr.length - 1;
+        
         while(start <= end) {
             int mid = start + (end - start) / 2;
+            int num = arr[mid];
             
-            if(nums[mid] == target)
+            // Check whether arr[mid] and target are in the same half or not
+            // This condition is shorter version of
+            // ((target < arr[0] && arr[mid] < arr[0]) || (target >= arr[0] && arr[mid] >= arr[0]))
+            if(!(target < arr[0]) == (arr[mid] < arr[0])) {
+                num = target < arr[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            
+            if(num == target) {
                 return mid;
+            }
             
-            if(nums[mid] < target) {
+            if(num < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
