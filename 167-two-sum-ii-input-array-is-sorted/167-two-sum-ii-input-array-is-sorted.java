9483079f2fbdbe5c0b093d927f232b34733507cb
Darshan Vaishya <66417026+DarshanVaishya@users.noreply.github.com>
2022-06-28 13:21:53 +0530

    Time: 2 ms (75.91%), Space: 50.5 MB (18.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        int n = arr.length;
+        int start = 0;
+        int end   = arr.length - 1;
         
-        for(int i = 0; i < n - 1; i++) {
-            int start = i + 1;
-            int end   = n - 1;
-            int res = binarySearch(arr, target - arr[i], start, end);
+        while(start < end) {
+            int sum = arr[start] + arr[end];
             
-            if(res != -1) {
-                return new int[] {i + 1, res + 1};
+            if(sum == target) {
+                return new int[] {start + 1, end + 1};
+            }
+            
+            if(sum < target) {
+                start++;
+            } else {
+                end--;
             }
         }
         
         return new int[] {-1, -1};
-    }
-    
-    public int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if(arr[mid] == target) {
-                return mid;
-            }
-            
-            if(arr[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        
-        return -1;
     }
 }
