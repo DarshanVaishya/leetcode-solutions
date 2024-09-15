32d9a6876cb72425505fdc5917bd3d44fcea2920
Darshan Vaishya <66417026+DarshanVaishya@users.noreply.github.com>
2024-09-15 19:06:29 -0400

    Time: 2 ms (83.35%), Space: 48.3 MB (35.24%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,35 +10,36 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-                    if(head==null||head.next==null) return;
-            
-            //Find the middle of the list
-            ListNode p1=head;
-            ListNode p2=head;
-            while(p2.next!=null&&p2.next.next!=null){ 
-                p1=p1.next;
-                p2=p2.next.next;
-            }
-            
-            //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
-            ListNode preMiddle=p1;
-            ListNode preCurrent=p1.next;
-            while(preCurrent.next!=null){
-                ListNode current=preCurrent.next;
-                preCurrent.next=current.next;
-                current.next=preMiddle.next;
-                preMiddle.next=current;
-            }
-            
-            //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
-            p1=head;
-            p2=preMiddle.next;
-            while(p1!=preMiddle){
-                preMiddle.next=p2.next;
-                p2.next=p1.next;
-                p1.next=p2;
-                p1=p2.next;
-                p2=preMiddle.next;
-            }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+
+        while(second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        ListNode first = head;
+        second = prev;
+        while(second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
     }
 }
