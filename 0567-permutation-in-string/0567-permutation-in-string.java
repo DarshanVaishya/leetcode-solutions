2dc9fe57f22727c599fafbaeebd19d445ba9c3c0
Darshan Vaishya <66417026+DarshanVaishya@users.noreply.github.com>
2024-09-30 15:27:14 -0400

    Time: 6 ms (86%), Space: 42 MB (98.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,36 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
-        // Find counts of s1
-        int[] targetC = new int[26];
-        for(int i = 0; i < s1.length(); i++) {
-            targetC[s1.charAt(i) - 'a']++;
-        }
-    
-        // Initialize the window counts for s2
-        int left = 0;
-        int[] currC = new int[26];
-        for(int i = 0; i < s1.length(); i++) {
-            currC[s2.charAt(i) - 'a']++;
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if(n1 > n2) return false;
+
+        int[] s1Counts = new int[26];
+        int[] s2Counts = new int[26];
+
+        for(int i = 0; i < n1; i++) {
+            s1Counts[s1.charAt(i) - 'a']++;
+            s2Counts[s2.charAt(i) - 'a']++;
         }
 
-        // Start looking for permutations
-        for(int right = s1.length(); right < s2.length(); right++) {
-            if(matchArr(currC, targetC)) {
-                return true;
-            }
-            currC[s2.charAt(left) - 'a']--;
-            currC[s2.charAt(right) - 'a']++;
-            left++;
+        if(Arrays.equals(s1Counts, s2Counts)) return true;
+
+        for(int i = n1; i < n2; i++) {
+            s2Counts[s2.charAt(i) - 'a']++;
+            s2Counts[s2.charAt(i - n1) - 'a']--;
+            if(Arrays.equals(s1Counts, s2Counts)) return true;
         }
 
-        return matchArr(currC, targetC);
-    }
-
-    public boolean matchArr(int[] arr1, int[] arr2) {
-        for(int i = 0; i < arr1.length; i++) {
-            if(arr1[i] != arr2[i]) return false;
-        }
-        return true;
+        return false;
     }
 }
